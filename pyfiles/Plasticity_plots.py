a2e339e15123d99a0fe656f45344ac3fa890169c
ARUN-PRAKASH-95 <arunprakash.fns@gmail.com>
2019-07-10 19:52:00 +0200

    parameters added

--- pyfiles/Plasticity_plots.py
+++ pyfiles/Plasticity_plots.py
@@ -1,44 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-step = 0.01
-t=np.arange(step,1,step)
-
-poissons_ratio = 0.2
-E = 0.2 
-mu = E/(2*(1+poissons_ratio))
-Lambda=poissons_ratio*E/((1-2*poissons_ratio)*(1+poissons_ratio))
-volumetric_strain = -0.01
-yield_stress = 200e-6 #N/micromm^2
-
-# Mesh generation
-Inner_radius = 5
-Outer_radius = 20
-n=10
-meshrefinementfactor = 5
-q=meshrefinementfactor**(1/(n-1))
-
-l=(Outer_radius-Inner_radius)*(1-q)/(1-meshrefinementfactor*q)
-rnode=Inner_radius
-coordinate=np.array([Inner_radius])
-
-for i in range(n):
-        rnode=rnode+l
-        coordinate=np.append(coordinate,rnode)
-        l=l*q
-
-C = np.array([[Lambda+2*mu,Lambda,Lambda],
-              [Lambda,Lambda+2*mu,Lambda],
-              [Lambda,Lambda,Lambda+2*mu]])
-
-#Tangent stiffness parameters
-a=np.eye(3)
-c=np.outer(a,a)
-Kay = 3*Lambda+2*mu
-b=np.eye(9)
-d=np.zeros((9,9))
-d[0,0]=1; d[1,3]=1;d[2,6]=1;d[3,1]=1;d[4,4]=1;d[5,7]=1;d[6,2]=1;d[7,5]=1;d[8,8]=1
-I = (1/2*(b+d))-(1/3*c)
+from Parameters import *
 
 
 Global_F_ext = np.zeros((n+1,1))
